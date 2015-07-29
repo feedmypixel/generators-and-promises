169d938718af96d55a07b461cfed9db55f07b3d4
Ben Chidgey <ben@feedmypixel.com>
2015-07-29 17:51:19 +0100

    improved error handling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,12 @@
 import user from './modules/user';
 import generatorUtil from './lib/generatorUtil';
 import display from './modules/display';
+import printError from './lib/utils/printError';
 
 generatorUtil.process(user.getSync)
-    .then(display.userDetails)
-    .catch(console.error.bind(console));
+    .then(display.userDetails, printError)
+    .catch(printError);
 
 generatorUtil.process(user.get)
-    .then(display.userDetails)
-    .catch(console.error.bind(console));
+    .then(display.userDetails, printError)
+    .catch(printError);
--- assets/javascript/modules/user.js
+++ assets/javascript/modules/user.js
@@ -6,26 +6,35 @@ const USER_STORE_URL = `${DATA_LOCATION}/userStore.json`;
 
 /**
  * Synchronous generator to provide user details by obtaining user id from one location and user details from another
- * @returns {{userId: *, userDetail: *}}
+ * @returns {*}
  */
 function *getSync() {
     /* NOTE I would typically use an endpoint to request the user data via ID but this is not what this
      investigation is about */
-    let user = yield request.get(USER_ID_URL);
-    let store = yield request.get(USER_STORE_URL);
+    try {
+        let user = yield request.get(USER_ID_URL);
+        let store = yield request.get(USER_STORE_URL);
 
-    return {user, store};
+        return Promise.resolve({user, store});
+    } catch (err) {
+        return Promise.reject(err);
+    }
 }
 
 /**
  * Parallel generator to provide user details and user store data from two locations
- * @returns {{userId: *, userDetail: *}}
+ * @returns {*}
  */
 function *get() {
-    let user = request.get(USER_ID_URL);
-    let store = request.get(USER_STORE_URL);
 
-    return {user: yield user, store: yield store};
+    try {
+        let user = request.get(USER_ID_URL);
+        let store = request.get(USER_STORE_URL);
+
+        return Promise.resolve({user: yield user, store: yield store});
+    } catch (err) {
+        return Promise.reject(err);
+    }
 }
 
 export default {
